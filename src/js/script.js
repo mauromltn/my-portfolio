4a756556a24793e0663d4eca2154b73fe5223c97
Mauro <116788542+mauromltn@users.noreply.github.com>
2024-09-26 16:41:44 +0000

    feat: Create cursor animation
    - Add a white dot shape cursor animation

---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', e => {
+  cursor.style.left = e.clientX + 'px';
+  cursor.style.top = e.clientY + 'px';
+});
