fedbd7e905d33a9b26be608743b31023b70eb4d7
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2024-09-28 16:12:00 +0200

    feat: expand cursor blob on certain elements
    The cursor will expand on certain elements which are, p, a, h1, h2, h3, button, and elments with class .big-cursor

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,21 @@
 const cursor = document.querySelector('.cursor');
+// Getting all the elments where the cursor should be bigger
+const bigCursorElments = document.querySelectorAll(".big-cursor, p, a, h1, h2, h3, button")
+
+
 document.addEventListener('mousemove', e => {
   cursor.style.left = e.clientX + 'px';
   cursor.style.top = e.clientY + 'px';
 });
+
+bigCursorElments.forEach((element) => {
+  element.addEventListener("mouseover", e => {
+    cursor.style.height = "100px"
+    cursor.style.width = "100px" 
+  })
+
+  element.addEventListener("mouseleave", e => {
+    cursor.style.height = "20px"
+    cursor.style.width = "20px" 
+  })
+})
