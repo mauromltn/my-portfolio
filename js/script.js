d101ce294825f1b950fbc71b763f1fa1a7bd0626
Mauro <116788542+mauromltn@users.noreply.github.com>
2025-02-19 00:30:21 +0000

    style: Improve footer layout and accessibility with updated labels and colors

--- js/script.js
+++ js/script.js
@@ -53,11 +53,9 @@ const lenis = new Lenis({
   duration: 2,
   smooth: true
 });
-
 // Use requestAnimationFrame to continuously update the scroll
 function raf(time) {
   lenis.raf(time);
   requestAnimationFrame(raf);
 }
-
 requestAnimationFrame(raf);
